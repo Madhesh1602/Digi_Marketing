af9b123d78dd147bad247a953324e4e461e1103a
Vijay <xSathyaB@nbf.ae>
2024-09-22 17:41:33 +0530

    Created class to describe an image

---
+++ scripts/ImageDescription.py
@@ -0,0 +1,42 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_community.document_loaders.image import UnstructuredImageLoader
+
+from Templates import SHORT_DESCRIPTION_TEMPLATE
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+# LLM Components
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
+short_description_prompt = PromptTemplate(
+    template=SHORT_DESCRIPTION_TEMPLATE, input_variables=["image"]
+)
+short_description_chain = short_description_prompt | llm
+
+
+class GetImageDescription:
+
+    def __init__(self, image_path):
+
+        global short_description_chain
+
+        self.image_path = image_path
+        self.chain = short_description_chain
+        self.image_data = None
+
+    def initialize_image_data(self):
+        loader = UnstructuredImageLoader(self.image_path)  # , mode="elements")
+        data = loader.load()
+        self.image_data = data
+
+    def generate_description(self):
+        self.initialize_image_data()
+        result = self.chain.invoke({"image": self.image_data})
+        return result.content
+
+    @classmethod
+    def describe(cls, image_path):
+        instance = cls(image_path)
+        description = instance.generate_description()
+        return description
---
+++ scripts/Templates.py
@@ -0,0 +1,7 @@
+SHORT_DESCRIPTION_TEMPLATE = """Give a short concise description of the given image.
+Ensure to include all the details that are visible in the image. Please do not
+hallucinate but only include the details that are visible in the image.
+
+image: 
+{image}
+"""
